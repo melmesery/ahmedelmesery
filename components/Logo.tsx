549c7b20bc30a8c5330a75de1affa092355d953a
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-10 00:40:58 +0200

    _new

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,3 +1,36 @@
+// import Image from "next/image";
+// import Link from "next/link";
+// import React, { useEffect, useState } from "react";
+// import useProfile from "../hooks/useProfile.tsx";
+
+// export default function Logo() {
+//   const [logoData, setLogoData] = useState<any>({ logo: null });
+
+//   useEffect(() => {
+//     useProfile().then((data) => {
+//       setLogoData(data);
+//     });
+//   }, []);
+
+//   const { logo } = logoData;
+
+//   return (
+//     <>
+//       {logo && (
+//         <Link href="/" className="cursor-pointer">
+//           <Image
+//             src={logo}
+//             alt="Ahmad-Elmesery-logo"
+//             width={55}
+//             height={55}
+//             // className="rounded-full"
+//           />
+//         </Link>
+//       )}
+//     </>
+//   );
+// }
+
 'use client';
 
 import Image from "next/image";
